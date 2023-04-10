324f9f89fd6f2fdacf7cddaa60bae2c7a6fc2fa5
Carlos Rodriguez <85375012+carlosrl19@users.noreply.github.com>
2023-04-10 08:52:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Dependencias necesarias
-sudo pacman -S make pkg-config yay picom feh dunst git rofi lxappearance brightnessctl dunstify
+sudo pacman -S make pkg-config yay picom feh dunst git rofi lxappearance brightnessctl
 
 # Crear las carpetas si no existen y mostrar mensaje si se crean
 if [ ! -d "/home/$USER/.themes" ] || [ ! -d "/home/$USER/.icons" ] || [ ! -d "/home/$USER/.config/i3" ] || [ ! -d "/home/$USER/.config/polybar" ] || [ ! -d "/home/$USER/.config/rofi" ] || [ ! -d "/home/$USER/.config/dunst" ] || [ ! -d "/home/$USER/.config/neofetch" ] || [ ! -d "/home/$USER/.config/picom" ]; then
