6fb9d1dd7a3b304048b30da12a3d3159b21d44db
Carlos Rodriguez <85375012+carlosrl19@users.noreply.github.com>
2023-04-10 07:52:57 -0600

    Se arregló error al instalar paquetes necesarios

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 sudo pacman -S make pkg-config yay picom feh dunst git rofi lxappearance
 
 # Crear las carpetas si no existen y mostrar mensaje si se crean
-if [ ! -d "/home/$USER/.themes" ] || [ ! -d "/home/$USER/.icons" ] || [ ! -d "/home/$USER/.config/i3" ] || [ ! -d "/home/$USER/.config/polybar" ] || [ ! -d "/home/$USER/.config/rofi" ] || [ ! -d "/home/$USER/.config/dunst" ] || [ ! -d "/home/$USER/.config/neofetch" ]; then
-smkdir -p /home/$USER/.themes /home/$USER/.icons /home/$USER/.config/i3 /home/$USER/.config/polybar /home/$USER/.config/rofi /home/$USER/.config/dunst /home/$USER/.config/neofetch
+if [ ! -d "/home/$USER/.themes" ] || [ ! -d "/home/$USER/.icons" ] || [ ! -d "/home/$USER/.config/i3" ] || [ ! -d "/home/$USER/.config/polybar" ] || [ ! -d "/home/$USER/.config/rofi" ] || [ ! -d "/home/$USER/.config/dunst" ] || [ ! -d "/home/$USER/.config/neofetch" ] || [ ! -d "/home/$USER/.config/picom" ]; then
+smkdir -p /home/$USER/.themes /home/$USER/.icons /home/$USER/.config/i3 /home/$USER/.config/polybar /home/$USER/.config/rofi /home/$USER/.config/dunst /home/$USER/.config/neofetch /home/$USER/.config/picom
   echo "Carpetas creadas exitosamente, procediendo a copiar los archivos..."
 fi
 
@@ -17,7 +17,7 @@ cp -r themes/* /home/$USER/.themes/
 cp -r icons/* /home/$USER/.icons/
 cp config/i3/* /home/$USER/.config/i3/
 cp -r config/polybar/* /home/$USER/.config/polybar/
-sudo cp config/picom/* /etc/xdg/picom.conf
+sudo cp config/picom/* /home/$USER/.config/picom/picom.conf
 cp config/rofi/* /home/$USER/.config/rofi/
 cp config/dunst/* /home/$USER/.config/dunst/
 cp config/config.conf /home/$USER/.config/neofetch/
@@ -25,9 +25,8 @@ cp wallpaper.jpg /home/$USER/Imágenes/
 
 # Configuración
 bash /home/$USER/.config/polybar/launch.sh --forest
-^C
 yay -S i3-gaps-rounded-git # instalando i3gaps rounded
-yay -S ttf-awesome-4
-yay -S ttf-awesome-5
+yay -S ttf-font-awesome-4
+yay -S ttf-font-awesome-5
 
 echo "Todo se ha copiado exitosamente"
